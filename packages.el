6a7cd27bcd0cde8e0b6b295f19c3995ef437fc1b
Alex Korablev <avkorablev@users.noreply.github.com>
2017-06-02 17:14:47 +0500

    initial commit

---
+++ packages.el
@@ -0,0 +1,19 @@
+;;; packages.el --- reverse-input-method layer packages file for Spacemacs.
+;;
+;; Author: Alexander Korablev
+;; URL: https://github.com/avkorablev/reverse-input-method-layer
+;;
+
+;;; Code:
+
+(defconst reverse-input-method-packages
+  '((reverse-input-method
+     :location (recipe
+                :fetcher github
+                :repo "avkorablev/reverse-input-method-package"
+                :files ("reverse-input-method.el")))))
+
+(defun reverse-input-method/init-reverse-input-method ()
+  (use-package reverse-input-method))
+
+;;; packages.el ends here
